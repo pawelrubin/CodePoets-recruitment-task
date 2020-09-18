fa1dec7fbff9fe752df66177f58e2818bbed3911
Paweł Rubin <pawelrubin19@gmail.com>
2020-09-19 01:07:26 +0200

    Seperate dockerfile for dev and production
    - proxy for API requests set in package.json
    - fastapi settings addapted

--- benfords_law/backend/app/main.py
+++ benfords_law/backend/app/main.py
@@ -1,8 +1,8 @@
 from fastapi import FastAPI
 
-app = FastAPI()
+app = FastAPI(openapi_url="/api/openapi.json", docs_url="/api/docs", redoc_url=None)
 
 
-@app.get("/")
+@app.get("/api/")
 async def root():
     return {"message": "Hello World 2137"}
